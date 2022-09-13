4fbfbe90c536fa99eb0a3fdd007aaca62e063bd2
Hussein Omar <husseinomar6190@gmail.com>
2022-09-13 16:26:58 +0300

    creating the hero & header  component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Experience from './components/Experience';
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section className='home' id='home'>
+      <div className='home-text'>
+        <h4>Hello</h4>
+        <h1>
+          I'M Hus<span>sein</span> <br /> Om<span>ar</span>
+        </h1>
+        <h3>Front-End Developer</h3>
+        <a href='#' className='btn'>
+          Download CV
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
