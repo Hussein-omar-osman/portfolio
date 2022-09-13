1c51341d2b7358bb165ce67b5c7c6976b6584a52
Hussein Omar <husseinomar6190@gmail.com>
2022-09-13 16:30:03 +0300

    creating the service section component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from './components/About';
 import Experience from './components/Experience';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Service from './components/Service';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Hero />
       <About />
+      <Service />
     </>
   );
 }
